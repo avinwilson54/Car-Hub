6243d3c19112dae09938644a44842754aed51de9
Avin Wilson <avinwilson54@gmail.com>
2023-09-04 22:08:45 +0530

    Navbar update

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Footer = () => {
+	return <footer>Footer</footer>;
+};
+
+export default Footer;
--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,11 @@
 import Hero from "./Hero";
-import Button from "./Button";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 export {
   Hero,
-  Button,
+  CustomButton,
+  Navbar,
+  Footer
 }
